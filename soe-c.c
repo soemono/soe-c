e4ab23fc1a7d98407f8304a41b4bc006f4498a41
soemono <53477278+soemono@users.noreply.github.com>
2020-03-02 15:55:34 +0900

    単項+/-の追加

--- soe-c.c
+++ soe-c.c
@@ -159,7 +159,9 @@ Node *new_node_num(int val) {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
+// 加減算
 Node *expr() {
     Node *node = mul();
 
@@ -174,20 +176,33 @@ Node *expr() {
     }
 }
 
+// 乗除算
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for(;;) {
         if (consume('*')) {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         } else if (consume('/')) {
-            node = new_node(ND_DIV, node ,primary());
+            node = new_node(ND_DIV, node ,unary());
         } else {
             return node;
         }
     }
 }
 
+// 単項+/-
+Node *unary(){
+    if (consume('+')) {
+        return primary();
+    }
+    if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+}
+
+// 括弧
 Node *primary() {
     if (consume('(')) {
         Node *node = expr();
@@ -197,6 +212,7 @@ Node *primary() {
 
     return new_node_num(expect_number());
 }
+
 
 void gen(Node *node) {
     if (node->kind == ND_NUM) {
